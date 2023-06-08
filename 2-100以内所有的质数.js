ee91e683fe686d1e17a9bc5431330d10f02a518c
mouuree <104833190+Mo-Yv@users.noreply.github.com>
2023-06-08 23:17:43 +0800

    Update 2-100以内所有的质数.js

--- 2-100以内所有的质数.js
+++ 2-100以内所有的质数.js
@@ -1,6 +1,6 @@
 for (let i = 2; i <= 100; i++) {
   var isPrime = true;
-  for (let j = 2; j < i; j++) {
+  for (let j = 2; j <= Math.sqrt(i); j++) {
     if (i % j == 0) {
       isPrime = false;
       break;
