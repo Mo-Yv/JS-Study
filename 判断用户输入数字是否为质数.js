ac2234116f0de1cb019b5dc126dcc12e2f9d7fa1
mouuree <104833190+Mo-Yv@users.noreply.github.com>
2023-06-08 18:26:11 +0800

    Update 判断用户输入数字是否为质数.js

--- 判断用户输入数字是否为质数.js
+++ 判断用户输入数字是否为质数.js
@@ -6,16 +6,16 @@ if (num <= 1 || num % 1 != 0) {
 } else {
   // 记录状态
   var isPrime = true;
-  //遍历从2到num-1的数字
+  // 遍历从2到num-1的数字
   for (let i = 2; i < num; i++) {
-    //与num取模,如果值为0,说明可以被整除,该数字不是质数
+    // 取模,如果值为0,说明num可以被i整除,num不是质数
     if (num % i == 0) {
-      //修改状态
+      // 修改状态
       flag = false;
-      //已经确定结果,后面的数字没必要继续便利了,结束循环
+      // 已经确定结果,后面的数字没必要继续遍历了,结束循环
       break;
     }
   }
-  //判断状态,打印结果
+  // 判断状态,打印结果
   isPrime ? document.write(num + "是质数") : document.write(num + "不是质数");
 }
