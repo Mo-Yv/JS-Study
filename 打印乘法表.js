beab72f7d7e14657d33c15d2d97804cbe134900d
mouuree <1782209330@qq.com>
2023-08-02 18:28:55 +0800

    Delete 打印乘法表.js

--- 打印乘法表.js
+++
@@ -1,8 +0,0 @@
-for (let i = 1; i < 10; i++) {
-  for (let j = 1; j <= i; j++) {
-    if (j <= i) {
-      document.write(j, "*", i, "=", i * j, "&nbsp;");
-    }
-  }
-  document.write("<br/>");
-}
