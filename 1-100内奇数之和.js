708108b66dd8b18fdaddccd1bb490a13c17acdf4
mouuree <1782209330@qq.com>
2023-08-26 13:54:52 +0800

    添加注释

--- 1-100内奇数之和.js
+++ 1-100内奇数之和.js
@@ -1,9 +1,11 @@
 //储存结果
-var sum = 0;
+let sum = 0;
+
 //遍历1-100的整数
-for(var i = 0;i < 100;i++){
+for(let i = 0;i < 100;i++){
   //判断是否可以被2整除
   if(!(i % 2)) sum += i;
 }
+
 //输出
 console.log(sum);
--- 2-100以内所有的质数.js
+++ 2-100以内所有的质数.js
@@ -1,10 +1,17 @@
+// 外层遍历: 从2开始循环遍历到100
 for (let i = 2; i <= 100; i++) {
+  // 初始化状态,代表当前数字是质数
   let isPrime = true;
+  // 内层遍历: 循环遍历从 2 到 i的平方根
   for (let j = 2; j <= Math.sqrt(i); j++) {
+    // 判断i是否可以被j整除
     if (!(i % j)) {
+      // 修改状态
       isPrime = false;
+      // 结束循环
       break;
     }
   }
+  // 输出打印
   if (isPrime) console.log(i);
 }
